1f2696a7cf4442155602b7cf2c07d888db4404b4
Michal Stefaniuk <sztepen@gmail.com>
2018-12-16 20:01:10 +0100

    fix

--- ts/app.ts
+++ ts/app.ts
@@ -1,8 +1,8 @@
 import 'xterm/dist/xterm.css'
-import * as Term from 'xterm';
+import * as Xterm from 'xterm';
 
 export class TestApp {
-    term: Term.Terminal;
+    term: any;
 
     private prompt() {
         this.term.write('\r\n$ ');
@@ -10,11 +10,41 @@ export class TestApp {
 
 
     constructor() {
-        this.term = new Term.Terminal();
+
+        let opts = {} as Xterm.ITerminalOptions;
+
+
+        opts.cursorBlink = true;
+        opts.cursorStyle = 'underline';
+        opts.disableStdin = false
+        //fontSize: 40
+
+        this.term = new Xterm.Terminal(opts);
+
+        let term = this.term;
 
         this.term.open(document.getElementById('terminal'));
 
         this.term.write("Mochii UI terminal. Type commands below...");
+
+        this.prompt();
+
+        term._core.register(term.addDisposableListener('key', (key: any, ev: any) => {
+            var printable = !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey;
+            if (ev.keyCode === 13) {
+
+                this.prompt();
+            }
+            else if (ev.keyCode === 8) {
+                // Do not delete the prompt
+                if (term.x > 2) {
+                    term.write('\b \b');
+                }
+            }
+            else if (printable) {
+                term.write(key);
+            }
+        }));
 
     }
 }
